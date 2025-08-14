6cacd620c15ae2f699bbec180aa2ed410c807867
Chera Mihiretu <63361469+chera-mihiretu@users.noreply.github.com>
2025-08-14 10:28:26 +0300

    Update LoginForm.tsx
    fixing email issue

--- web/src/components/login/LoginForm.tsx
+++ web/src/components/login/LoginForm.tsx
@@ -74,7 +74,7 @@ const LoginForm: React.FC = () => {
       <div className="mb-5">
         <input
           type="email"
-          placeholder="Enter your university email"
+          placeholder="Enter your email"
           value={email}
           onChange={e => setEmail(e.target.value)}
           className={`w-full px-4 py-3 rounded-lg font-poppins text-base outline-none mb-2 bg-inputBg text-[#171717] shadow-md transition-all duration-200 border-[1.5px] ${error ? 'border-error shadow-[0_0_0_2px_rgba(211,47,47,0.2),0_2px_8px_#e0e0e0]' : 'border-inputBorder shadow-[0_2px_8px_#e0e0e0]'}`}
